041d158be8b4b15c40eba2a76b1daa736dd0690f
DonMB77 <purposefuldrifter@gmail.com>
2025-02-21 11:51:47 +0100

    initial init

---
+++ src/main/java/com/sda_se_iban_validator/sda_se_iban_validator/SdaSeIbanValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.sda_se_iban_validator.sda_se_iban_validator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdaSeIbanValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdaSeIbanValidatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sda_se_iban_validator/sda_se_iban_validator/SdaSeIbanValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda_se_iban_validator.sda_se_iban_validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdaSeIbanValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
